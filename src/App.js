a65433cfa234537f1e2d48d8a95b7babb90d8ccd
Niels Louwes <76694402+NielsLouwes@users.noreply.github.com>
2022-04-12 14:15:44 +0200

    Setting up components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React, { useState } from "react";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 import "./styles.css";
 
 export default function App() {
+  const [todoValue, setTodoValue] = useState("");
   return (
-    <div>
+    <div className="App">
       <header>
         <h1>To Do List </h1>
+        <Form />
+        <TodoList />
       </header>
     </div>
   );
