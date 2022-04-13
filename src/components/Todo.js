264a5e2054c9819ae9e1e9c52e89b970b818f5a6
Niels Louwes <76694402+NielsLouwes@users.noreply.github.com>
2022-04-13 08:35:30 +0200

    Added conmpletion functionality for todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,24 +1,32 @@
 import React from "react";
 
 const Todo = ({ text, todos, todo, setTodos }) => {
+  //the delete hanlders filters based on a NON match between the element ID and the singular todoItem we passed on through props
   const deleteHandler = () => {
     setTodos(todos.filter((element) => element.id !== todo.id));
   };
 
-  //deleting an item
-  // 1. we need access to the array of objects
-  // 2. we need to check that the button we clicked matches ID of the item in aray
-  // 3. when found, remove it from array (find or filter function?)
-  // SUDO code
-  // const removeItem = (nameOfItem = {
-  //   ...todos.indexOf('textName');
-  //   //remove that item from array
-  // });
+  //For completing items, we need to map through, find a match with our select todo , return a new object with its existing attributes by spreading ... and then change completed to TRUE.
+  const completeHandler = () => {
+    setTodos(
+      todos.map((element) => {
+        if (element.id === todo.id) {
+          return {
+            ...element,
+            completed: true
+          };
+        }
+        return element;
+      })
+    );
+  };
 
   return (
     <div className="todo">
-      <li className="todo-item">{text}</li>
-      <button className="complete-btn">
+      <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
+        {text}
+      </li>
+      <button onClick={completeHandler} className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
       <button onClick={deleteHandler} className="trash-btn">
